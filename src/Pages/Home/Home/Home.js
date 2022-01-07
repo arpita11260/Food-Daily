39ca47b08de342f0cb0e051e565fbfcf4a4df9fd
arpita.sristy <arpita.sristy18@gmail.com>
2022-01-07 16:24:21 +0600

    added checkout ---by sugata

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import AboutUs from '../../../Aboutus/AboutUs';
 import Contact from '../../../Contract/Contract';
-import Datepicker from '../../../Datepicker/Datepicker';
 import Footer from '../../../Footer/Footer';
-import Booking from '../../Booking/Booking/Booking';
 import Banner from '../Banner/Banner';
 import Reviews from '../Reviews/Reviews';
-import Services from '../Services/Services';
 import Faq from './Faq/Faq';
 
 const Home = () => {
